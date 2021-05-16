84ad9f2d978200721d736dcf7b3cb4ab108bee88
Allan Niemerg <niemerg@gmail.com>
2021-05-16 15:19:31 -0400

    removed dependencies by adding files to contract folder

--- contracts/mocks/MockERC20.sol
+++ contracts/mocks/MockERC20.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.1;
 
-import "@yield-protocol/utils-v2/contracts/token/ERC20.sol";
+import "../token/ERC20.sol";
 
 contract MockERC20 is ERC20 {
   constructor() ERC20("Mock Token", "MOCK", 18) {
